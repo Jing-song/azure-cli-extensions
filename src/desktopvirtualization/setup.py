9c52bd705f2273a5ccf5e21aff67ab7aee74d083
kai ru <69238381+kairu-ms@users.noreply.github.com>
2021-07-13 14:58:30 +0800

    add missing help for top level command group (#3547)
    * add missing help
    
    * Update setup.py
    
    * Update src/dnc/azext_dnc/manual/_help.py
    
    * Update src/guestconfig/azext_guestconfig/manual/_help.py
    
    * Update src/healthcareapis/azext_healthcareapis/manual/_help.py
    
    * Update src/logic/azext_logic/manual/_help.py
    
    * Update src/portal/azext_portal/manual/_help.py
    
    * Update src/securityinsight/azext_sentinel/manual/_help.py
    
    * Update src/stack-hci/azext_stack_hci/manual/_help.py
    
    * fix linter issues

--- src/desktopvirtualization/setup.py
+++ src/desktopvirtualization/setup.py
@@ -8,15 +8,13 @@
 
 from codecs import open
 from setuptools import setup, find_packages
-try:
-    from azure_bdist_wheel import cmdclass
-except ImportError:
-    from distutils import log as logger
-    logger.warn("Wheel is not available, disabling bdist_wheel hook")
 
-# TODO: Confirm this is the right version number you want and it matches your
 # HISTORY.rst entry.
-VERSION = '0.1.3'
+VERSION = '0.1.1'
+try:
+    from .manual.version import VERSION
+except ImportError:
+    pass
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -26,16 +24,17 @@ CLASSIFIERS = [
     'Intended Audience :: System Administrators',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'License :: OSI Approved :: MIT License',
 ]
 
-# TODO: Add any additional SDK dependencies here
 DEPENDENCIES = []
+try:
+    from .manual.dependency import DEPENDENCIES
+except ImportError:
+    pass
 
 with open('README.md', 'r', encoding='utf-8') as f:
     README = f.read()
@@ -43,17 +42,16 @@ with open('HISTORY.rst', 'r', encoding='utf-8') as f:
     HISTORY = f.read()
 
 setup(
-    name='databox',
+    name='desktopvirtualization',
     version=VERSION,
-    description='Microsoft Azure Command-Line Tools DataBox Extension',
-    # TODO: Update author and email, if applicable
+    description='Microsoft Azure Command-Line Tools DesktopVirtualizationAPIClient Extension',
     author='Microsoft Corporation',
     author_email='azpycli@microsoft.com',
-    url='https://github.com/Azure/azure-cli-extensions/tree/master/src/databox',
+    url='https://github.com/Azure/azure-cli-extensions/tree/master/src/desktopvirtualization',
     long_description=README + '\n\n' + HISTORY,
     license='MIT',
     classifiers=CLASSIFIERS,
     packages=find_packages(),
     install_requires=DEPENDENCIES,
-    package_data={'azext_databox': ['azext_metadata.json']},
+    package_data={'azext_desktopvirtualization': ['azext_metadata.json']},
 )
